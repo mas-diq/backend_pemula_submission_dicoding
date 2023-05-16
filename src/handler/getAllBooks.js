4611a6ebd3d67b364cdd639cfc61006a1af2282a
Muhammad Shiddiq Fathullah <shiddiqmu4@gmail.com>
2023-05-16 14:46:29 +0700

    add query params

--- src/handler/getAllBooks.js
+++ src/handler/getAllBooks.js
@@ -1,16 +1,37 @@
 const books = require("../books.js");
 
-const getAllBooks = () => {
-    return {
+const getAllBooks = (request, h) => {
+    const query = request.query;
+    let result = books;
+
+    if (query.name) {
+        const paramsName = query.name.toLowerCase();
+        result = books.filter(data => data.name.toLowerCase().includes(paramsName))
+    }
+
+    if (query.reading != null) {
+        const paramsReading = (query.reading === '1');
+        result = books.filter(data => data.reading === paramsReading)
+    }
+
+    if (query.finished != null) {
+        const paramFinished = query.finished === '1';
+        result = books.filter(data => data.finished === paramFinished)
+    }
+
+    const returnValue = result.map(data => ({
+        id: data.id,
+        name: data.name,
+        publisher: data.publisher
+    }))
+
+    const response = h.response({
         status: 'success',
         data: {
-            books: books.map(book => ({
-                id: book.id,
-                name: book.name,
-                publisher: book.publisher
-            })),
+            books: returnValue,
         }
-    }
+    })
+    return response
 };
 
 module.exports = {
